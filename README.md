1036890b466785980a311a285e648d3374ef12e0
Nick HS <nickhs@umich.edu>
2013-10-27 20:45:00 -0400

    Type change of README and run_tests

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-../base/README.md
+# EECS 281 Community Test Cases
+
+This is a repository of shared test cases for 281 projects. It includes a test
+runner (`run_tests.py`) and a number of test cases in `tests` and their relevant
+output in `output`. `tests.json` is the glue that sticks the parts together.
+
+If you use these tests and spot a misssing edge case consider adding it in to
+the repo!
+
+# Quickstart
+
+Clone this repository:
+
+    git clone https://github.com/nickhs/EECS281-Project3-Tests
+
+Run the tests:
+
+    python run_tests.py my_program_executable
+
+Where `my_program_executable` is the location of your program.
+
+# Contributing
+
+Make your changes to the repository and submit a
+pull request. Someone will look at it shortly and
+merge it in!
+
+## Step One
+
+Add your test cases to the `tests` folder. Name it something unique.
+
+## Step Two
+
+Add the test case output to the `output` folder. Name it the same
+thing you named your test case so others can track it down easier.
+
+## Step Three
+
+Add your new test case to tests.json by adding it in the `tests` block.
+Make sure it includes the following information:
+
+    "test1": { // the name of the test, make this unique
+        "email": "nickhs@umich.edu", // your email address
+        "added": "27-Oct-2013", // the date it was added (today's date)
+        "test": "-v 100 -c PAIRING tests/test-4-PAIRING.txt", // the relevant flags and path to the test case
+        "output": "output/test-4-PAIRING.txt" // the path to the output
+    },
+
+## Step Four
+
+Test it. Run `run_tests.py` and make sure everything appears to be working.
+
+## Step Five
+
+Submit a pull request back to the repository. Pat yourself on the back, you're a saint..
+
+# FAQ
+
+1) One of these test cases is incorrect? How do I remove it?
+
+    Open up an issue and we'll get right into looking into it.
+
+2) How do I know someone won't steal my tests?
+
+    It's against the Engineering Honour Code to submit a test case
+    that is not original (i.e. not yours) to the autograder.
+    Git/Github does all the book keeping for us on who submitted what
+    that we can turn over to the honour council if required.
+
+    Please do NOT submit tests cases that aren't yours. We do reserve
+    the right to report any and all suspected honour code violations.
+
+3) It told me to report an issue - what do I do?
+
+    Open a Github issue and we'll look into it right away. Let us know
+    what output you got from `run_tests.py` and make sure you're on latest!
--- run_tests.py
+++ run_tests.py
@@ -1,1 +1,108 @@
-../base/run_tests.py
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import json
+import subprocess
+import sys
+
+
+def run():
+    data = open('tests.json').read()
+
+    try:
+        data = json.loads(data)
+    except ValueError:
+        print "[x] Failed to parse tests.json! Exiting."
+        print "[.] You should report this issue."
+        return
+
+    if len(sys.argv) == 1:
+        print "[x] You need to specify the location of the program"
+        _help()
+        return
+
+    if len(sys.argv[1]) == '-h':
+        _help()
+        return
+
+    program = sys.argv[1]
+    failed_cases = []
+    for name, item in data.get('tests', {}).iteritems():
+        ret = run_test(program, name, item)
+        if not ret:
+            print "[/] Passed %s [%s]" % (name, item.get('email', 'No Email'))
+        else:
+            print "[x] Failed %s [%s]" % (name, item.get('email', 'No Email'))
+            failed_cases.append(ret)
+
+    if not failed_cases:
+        print "[/] Passed ALL tests"
+        return
+
+    print "You failed the following %d cases:" % len(failed_cases)
+    for item in failed_cases:
+        print "[.] %s: %s" % (item['reason'], item['cmd'])
+        print item['details']
+        print
+
+
+def run_test(program, name, item):
+    if 'test' not in item:
+        print "[x] Skipping %s - no flags. Please report this" % name
+        return
+
+    cmd = "%s %s" % (program, item['test'])
+
+    print "[.] Running %s (%s)" % (name, cmd)
+
+    try:
+        devnull = open('/dev/null', 'w')
+        ret = subprocess.check_output(cmd, shell=True, stderr=devnull)
+    except subprocess.CalledProcessError as e:
+        return {
+            'error': True,
+            'reason': 'Program Crashed',
+            'details': e,
+            'email': item.get('email', 'No Email'),
+            'cmd': cmd
+        }
+
+    if 'output' not in item:
+        print "[x] No output for test %s. Please report this" % name
+        return
+
+    actual_output = open(item['output']).read().strip().split('\n')
+    output = ret.strip().split('\n')
+
+    greater = len(actual_output)
+    if len(actual_output) != len(output):
+        if len(actual_output) > len(output):
+            greater = len(actual_output)
+        else:
+            greater = len(output)
+
+    i = 0
+    while (i < greater - 1):
+        if not actual_output[i] == output[i]:
+            return {
+                'error': True,
+                'reason': 'Output Difference',
+                'details': 'Line difference on line %s. \nActual Output: %s\nYour Output: %s' % (i, actual_output[i], output[i]),
+                'email': item.get('email', 'No Email'),
+                'cmd': cmd
+            }
+
+        i = i + 1
+
+
+def _help():
+    print "EECS 281 Community Grader"
+    print "python run_tests path/to/your/program"
+    print "Example: python run_tests.py ../proj2/MineEscape"
+    print
+    print "-h Prints this messasge"
+
+
+if __name__ == "__main__":
+    run()
